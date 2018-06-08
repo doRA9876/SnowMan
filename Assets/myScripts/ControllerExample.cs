9becb2563f0af8c837715accb17d0199184c106d
DESKTOP-1IUP82B\Arihara <9876dora@gmail.com>
2018-06-08 14:54:22 +0900

    initial commit

---
+++ Assets/myScripts/ControllerExample.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+
+public class ControllerExample : MonoBehaviour
+{
+  private GameObject controllerR;
+  private GameObject cube1;
+
+  private void Start()
+  {
+    controllerR = GameObject.Find("Controller (right)");
+    cube1 = GameObject.Find("Cube1");
+  }
+
+  void Update()
+  {
+    var trackedObject = GetComponent<SteamVR_TrackedObject>();
+    var device = SteamVR_Controller.Input((int)trackedObject.index);
+
+    if (device.GetTouchDown(SteamVR_Controller.ButtonMask.Trigger))
+    {
+      Debug.Log("トリガーを浅く引いた");
+    }
+    if (device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger))
+    {
+      Debug.Log("トリガーを深く引いた");
+    }
+
+    if (device.GetPress(SteamVR_Controller.ButtonMask.Trigger))
+    {
+      Debug.Log("トリガーを握っている");
+      var Pos3 = controllerR.transform.position;
+      Debug.Log(Pos3.x);
+      Debug.Log(Pos3.y);
+      Debug.Log(Pos3.z);
+      GripCube();
+    }
+
+    if (device.GetTouchUp(SteamVR_Controller.ButtonMask.Trigger))
+    {
+      Debug.Log("トリガーを離した");
+    }
+    if (device.GetPressDown(SteamVR_Controller.ButtonMask.Touchpad))
+    {
+      Debug.Log("タッチパッドをクリックした");
+    }
+    if (device.GetPress(SteamVR_Controller.ButtonMask.Touchpad))
+    {
+      Debug.Log("タッチパッドをクリックしている");
+    }
+    if (device.GetPressUp(SteamVR_Controller.ButtonMask.Touchpad))
+    {
+      Debug.Log("タッチパッドをクリックして離した");
+    }
+    if (device.GetTouchDown(SteamVR_Controller.ButtonMask.Touchpad))
+    {
+      Debug.Log("タッチパッドに触った");
+    }
+    if (device.GetTouchUp(SteamVR_Controller.ButtonMask.Touchpad))
+    {
+      Debug.Log("タッチパッドを離した");
+    }
+    if (device.GetPressDown(SteamVR_Controller.ButtonMask.ApplicationMenu))
+    {
+      Debug.Log("メニューボタンをクリックした");
+    }
+    if (device.GetPressDown(SteamVR_Controller.ButtonMask.Grip))
+    {
+      Debug.Log("グリップボタンをクリックした");
+    }
+
+    if (device.GetTouch(SteamVR_Controller.ButtonMask.Trigger))
+    {
+      //Debug.Log("トリガーを浅く引いている");
+    }
+    if (device.GetPress(SteamVR_Controller.ButtonMask.Trigger))
+    {
+      //Debug.Log("トリガーを深く引いている");
+    }
+    if (device.GetTouch(SteamVR_Controller.ButtonMask.Touchpad))
+    {
+      //Debug.Log("タッチパッドに触っている");
+    }
+  }
+
+  private void GripCube()
+  {
+    cube1.transform.position = controllerR.transform.position;
+  }
+}
